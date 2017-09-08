42df4a8118ccaaa8dd6013d318245866c56e94ae
Brian Hopkins <brianhh1230@gmail.com>
2017-09-08 17:16:43 -0400

    updating location

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BOT_LOG_LEVEL = logging.DEBUG
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 BACKEND = 'Mattermost'
-BOT_EXTRA_BACKEND_DIR = os.path.join(BASE_DIR, 'backends')
+BOT_EXTRA_BACKEND_DIR = os.path.join(BASE_DIR, 'plugins')
 
 login = os.getenv('login')
 password = os.getenv('password')
