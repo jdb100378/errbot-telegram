3d7919da641a3295a9205bde53baa707252ef37e
Brian Hopkins <brianhh1230@gmail.com>
2017-09-08 17:59:00 -0400

    updating

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ BOT_LOG_LEVEL = logging.DEBUG
 BASE_DIR = '/app'
 
 BACKEND = 'Mattermost'
-BOT_EXTRA_BACKEND_DIR = '/app/plugins'
+BOT_EXTRA_BACKEND_DIR = '/app/backend'
 
 login = os.getenv('login')
 password = os.getenv('password')
